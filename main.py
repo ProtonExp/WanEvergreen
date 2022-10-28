8d0e42a860b3021ede58d1284188ca0755329be9
Tristan Cremer <116879546+ProtonExp@users.noreply.github.com>
2022-10-28 09:45:03 +0000

    Main.py was updated with new code

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+urank = ('cadet')
+rank = urank
 name = input('What is your name?\n')
-print('Hi, %s.' % name)
+if name == ('Layla') :
+  print('Go away')
+  exit()
+else :
+  print('Hi, %s.' % name)
+print(' ')
 print('You need to eliminate the targets')
+print(' ')
 print('Will you do it?')
+print(' ')
 ty = input('\n')
-if ty == ('I accept') :
-  print('The targets are Layla,Madison,Angelica,Emily')
+if ty == ('No') :
+  print('You do not belong here...')
+if ty == ('Yes') :
+  print('The targets are Those who look down on you')
   ud = input('\n')
   if ud == ('Understood') :
-     print('You are to surpass Them and Break them')
+     print('You are to surpass Them and Break them Mentally')
+     print(' ')
+     print('Make sure you dont get caught')
+     print(' ')
+     print('Good luck, %s.' % rank)
+
+
+    
  
 
