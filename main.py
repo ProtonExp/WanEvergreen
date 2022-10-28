e25c0470aeaaf31df4fcc0415327610f56c19d40
Tristan Cremer <>
2022-10-28 09:27:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name = input('What is your name?\n')
+print('Hi, %s.' % name)
+print('You need to eliminate the targets')
+print('Will you do it?')
+ty = input('\n')
+if ty == ('I accept') :
+  print('The targets are Layla,Madison,Angelica,Emily')
+  ud = input('\n')
+  if ud == ('Understood') :
+     print('You are to surpass Them and Break them')
+ 
+
